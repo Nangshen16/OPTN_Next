1771831b38843c98c80c8dcfe3ad89351b6567f8
nshrachel <shen.icecream94@gmail.com>
2023-07-28 10:14:24 -0400

    added navbar

---
+++ src/components/export.jsx
@@ -0,0 +1,4 @@
+export {default as Nav} from './Nav';
+export {default as CryptoCurrencies} from './Cryptocurrencies';
+export {default as Exchanges} from './Exchanges';
+export {default as QrCode} from './QrCode';
